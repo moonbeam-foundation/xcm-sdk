cd894dd33e767d970f4a50c089258f15ca0d9e12
Elmar Kenigs <elmar@purestake.com>
2022-07-27 12:28:34 +0200

    Added xcmPallet tests

---
+++ packages/config/src/extrinsic/xcmPallet/xcmPallet.test.ts
@@ -0,0 +1,37 @@
+import { xcmPallet } from './xcmPallet';
+import { Chain, MoonChain } from '../../constants';
+import { PolkadotXcmExtrinsicSuccessEvent } from '../polkadotXcm';
+
+describe('xcmPallet', () => {
+  const amount = 1000n;
+  const account = '0xeF46c7649270C912704fB09B75097f6E32208b85';
+  const chain = {
+    chain: MoonChain.MoonbaseAlpha,
+    name: 'Moonbase Alpha',
+    ws: 'wss://wss.api.moonbase.moonbeam.network',
+    parachainId: 1000,
+  };
+  const origin = {
+    chain: Chain.AlphanetRelay,
+    name: 'Alphanet Relay',
+    ws: 'wss://frag-moonbase-relay-rpc-ws.g.moonbase.moonbeam.network',
+    weight: 1_000_000_000,
+    parachainId: 0,
+  };
+  const extrinsic = xcmPallet(chain);
+
+  describe('limitedReserveTransferAssets', () => {
+    const cfg = extrinsic
+      .limitedReserveTransferAssets()
+      .successEvent(PolkadotXcmExtrinsicSuccessEvent.Attempted)
+      .origin(origin);
+
+    it('should be correct config', () => {
+      expect(cfg).toMatchSnapshot();
+    });
+
+    it('should get correct params', () => {
+      expect(cfg.getParams(account, amount)).toMatchSnapshot();
+    });
+  });
+});
