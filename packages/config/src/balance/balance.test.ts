bb075833461cb61d826e6b9bd7753e0cd3793d8d
Elmar Kenigs <elmar@purestake.com>
2022-07-27 09:57:46 +0200

    Added tests for balance and withdraw

--- packages/config/src/balance/balance.test.ts
+++ packages/config/src/balance/balance.test.ts
@@ -2,23 +2,59 @@ import { createBalanceBuilder } from './balance';
 
 describe('Balance', () => {
   const account = '<ACCOUNT>';
-  const builder = createBalanceBuilder();
+  const balance = createBalanceBuilder();
 
   describe('assets', () => {
-    it('should return assets balance config with params', () => {
-      const cfg = builder.assets(1);
+    const cfg = balance.assets(1);
 
+    it('should return assets balance config', () => {
       expect(cfg).toMatchSnapshot();
+    });
+
+    it('should return assets balance params', () => {
       expect(cfg.getParams(account)).toMatchSnapshot();
     });
   });
 
   describe('min', () => {
-    it('should return min balance config with params', () => {
-      const cfg = builder.min(1);
+    const cfg = balance.min(1);
 
+    it('should return min balance config', () => {
       expect(cfg).toMatchSnapshot();
+    });
+
+    it('should return min balance config params', () => {
       expect(cfg.getParams()).toMatchSnapshot();
+    });
+  });
+
+  describe('system', () => {
+    const cfg = balance.system();
+
+    it('should return system balance config', () => {
+      expect(cfg).toMatchSnapshot();
+    });
+
+    it('should return system balance config params', () => {
+      expect(cfg.getParams(account)).toMatchSnapshot();
+    });
+  });
+
+  describe('tokens', () => {
+    const cfg = balance.tokens('MOVR');
+
+    it('should return tokens balance config', () => {
+      expect(cfg).toMatchSnapshot();
+    });
+
+    it('should return tokens balance config with Token params', () => {
+      expect(cfg.getParams(account)).toMatchSnapshot();
+    });
+
+    it('should return tokens balance config with ForeignAsset params', () => {
+      const cfg2 = balance.tokens(5);
+
+      expect(cfg2.getParams(account)).toMatchSnapshot();
     });
   });
 });
---
+++ packages/config/src/withdraw/withdraw.test.ts
@@ -0,0 +1,34 @@
+import { createWithdrawBuilder } from './withdraw';
+
+describe('Withdraw', () => {
+  const account = '0xeF46c7649270C912704fB09B75097f6E32208b85';
+  const withdraw = createWithdrawBuilder();
+
+  describe('xTokens', () => {
+    const cfg = withdraw.xTokens({
+      balance: '<BALANCE>' as any,
+      destination: { parachainId: 1000 } as any,
+      existentialDeposit: 100_000_000_000,
+      feePerWeight: 8,
+    });
+
+    it('should be correct withdraw config', () => {
+      expect(cfg).toMatchSnapshot();
+    });
+
+    it('should get params with parachain id', () => {
+      expect(cfg.getParams(account)).toMatchSnapshot();
+    });
+
+    it('should get params without parachain id', () => {
+      const cfg2 = withdraw.xTokens({
+        balance: '<BALANCE>' as any,
+        destination: {} as any,
+        existentialDeposit: 100_000_000_000,
+        feePerWeight: 8,
+      });
+
+      expect(cfg2.getParams(account)).toMatchSnapshot();
+    });
+  });
+});
