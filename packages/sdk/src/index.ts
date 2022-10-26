c1dbf8a3769b6f07822e14a1804522e981278659
Elmar Kenigs <elmar@purestake.com>
2022-10-26 16:41:34 +0200

    Small fixes

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,3 +1,11 @@
+export { ExtrinsicStatus } from '@moonbeam-network/xcm-utils';
+export type {
+  ExtrinsicEvent,
+  ExtrinsicEventsCallback,
+  ExtrinsicFailedEvent,
+  ExtrinsicSentEvent,
+  ExtrinsicSuccessEvent,
+} from '@moonbeam-network/xcm-utils';
 export * from './contracts';
 export * from './polkadot';
 export * from './sdk';
