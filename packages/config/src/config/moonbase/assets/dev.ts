8a0f877a8caf8dab86eb8cb5a6454ee97939b22d
Francisco Gamundi <francisco@purestake.com>
2022-12-06 16:46:04 +0100

    Removed unused Alphanet chains and assets

--- packages/config/src/config/moonbase/assets/dev.ts
+++ packages/config/src/config/moonbase/assets/dev.ts
@@ -15,29 +15,14 @@ import {
 import { MoonbaseXcmConfig } from '../moonbase.interfaces';
 
 const asset = assets[AssetSymbol.DEV];
-const clover = chains[ChainKey.CloverAlphanet];
 const pioneer = chains[ChainKey.BitCountryPioneer];
 
-const cloverDevId = getMoonAssetId(clover);
 const pioneerDevId = getMoonAssetId(pioneer);
 
 export const DEV: MoonbaseXcmConfig = {
   asset,
   origin: moonbase,
   deposit: {
-    [clover.key]: {
-      source: clover,
-      balance: balance.assets(cloverDevId),
-      sourceFeeBalance: balance.system(),
-      extrinsic: extrinsic
-        .xTokens()
-        .transfer()
-        .successEvent(XTokensExtrinsicSuccessEvent.TransferredMultiAssets)
-        .origin(clover)
-        .asset({
-          [XTokensExtrinsicCurrencyTypes.OtherReserve]: cloverDevId,
-        }),
-    },
     [pioneer.key]: {
       source: pioneer,
       balance: balance.tokens().fungibleToken(pioneerDevId),
@@ -51,11 +36,6 @@ export const DEV: MoonbaseXcmConfig = {
     },
   },
   withdraw: {
-    [clover.key]: withdraw.xTokens({
-      balance: balance.assets(cloverDevId),
-      destination: clover,
-      feePerWeight: 50_000,
-    }),
     [pioneer.key]: withdraw.xTokens({
       balance: balance.tokens().fungibleToken(pioneerDevId),
       destination: pioneer,
