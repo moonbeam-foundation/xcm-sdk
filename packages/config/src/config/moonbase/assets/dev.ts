9738e67d676a5f1bd5bd014328f5f93916956c33
Francisco Gamundi <francisco@purestake.com>
2022-12-12 14:11:41 +0100

    Removed Dev BitCountry config

--- packages/config/src/config/moonbase/assets/dev.ts
+++ packages/config/src/config/moonbase/assets/dev.ts
@@ -1,45 +1,12 @@
-import { AssetSymbol, ChainKey } from '../../../constants';
-import {
-  XTokensExtrinsicCurrencyTypes,
-  XTokensExtrinsicSuccessEvent,
-} from '../../../extrinsic';
-import { getMoonAssetId } from '../../config.utils';
-import {
-  assets,
-  balance,
-  chains,
-  extrinsic,
-  moonbase,
-  withdraw,
-} from '../moonbase.common';
+import { AssetSymbol } from '../../../constants';
+import { assets, moonbase } from '../moonbase.common';
 import { MoonbaseXcmConfig } from '../moonbase.interfaces';
 
 const asset = assets[AssetSymbol.DEV];
-const pioneer = chains[ChainKey.BitCountryPioneer];
-
-const pioneerDevId = getMoonAssetId(pioneer);
 
 export const DEV: MoonbaseXcmConfig = {
   asset,
   origin: moonbase,
-  deposit: {
-    [pioneer.key]: {
-      source: pioneer,
-      balance: balance.tokens().fungibleToken(pioneerDevId),
-      sourceFeeBalance: balance.system(),
-      extrinsic: extrinsic
-        .xTokens()
-        .transfer()
-        .successEvent(XTokensExtrinsicSuccessEvent.TransferredMultiAssets)
-        .origin(pioneer)
-        .asset({ [XTokensExtrinsicCurrencyTypes.FungibleToken]: pioneerDevId }),
-    },
-  },
-  withdraw: {
-    [pioneer.key]: withdraw.xTokens({
-      balance: balance.tokens().fungibleToken(pioneerDevId),
-      destination: pioneer,
-      feePerWeight: 50_000,
-    }),
-  },
+  deposit: {},
+  withdraw: {},
 };
