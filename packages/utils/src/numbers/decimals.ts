e1ab66abf5a8b9933eac0b796c8518e738595941
Mario J Maurello <marioj@purestake.com>
2023-02-27 13:13:29 +0100

    change return type of toDecimal function to 'Big'

--- packages/utils/src/numbers/decimals.ts
+++ packages/utils/src/numbers/decimals.ts
@@ -5,12 +5,12 @@ export function toDecimal(
   decimals: number,
   maxDecimal = 6,
   roundType?: RoundingMode,
-): string {
+): Big {
   const dividend = Big(number.toString().replace(/[^0-9]/g, ''));
   const divisor = Big(10).pow(decimals);
   const result = dividend.div(divisor).round(maxDecimal, roundType);
 
-  return result.toFixed();
+  return result;
 }
 
 export function toBigInt(amount: string | number, decimals: number): bigint {
