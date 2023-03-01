dd90357367f910aa7b6f14e852e47dc4ffe97186
Mario J Maurello <marioj@purestake.com>
2023-03-01 10:09:07 +0100

    use constant as parameter

--- packages/utils/src/numbers/decimals.ts
+++ packages/utils/src/numbers/decimals.ts
@@ -17,7 +17,7 @@ export function toBigInt(amount: string | number, decimals: number): bigint {
   const multiplier = Big(10).pow(decimals);
   const result = Big(amount).mul(multiplier);
 
-  return BigInt(result.toFixed(0, 0));
+  return BigInt(result.toFixed(0, Big.roundDown));
 }
 
 export function hasDecimalOverflow(fl: number | string, maxDecimal: number) {
