b0c2919baa763a33059b32d467cb7594601941fd
Elmar Kenigs <elmar@purestake.com>
2023-05-01 17:34:15 +0300

    --wip-- [skip ci]

--- packages/config/src/configs/index.ts
+++ packages/config/src/configs/index.ts
@@ -1,10 +1,12 @@
 import { ChainConfig } from '../ChainConfig';
 import { acalaConfig } from './acala';
 import { alphanetRelayConfig } from './alphanetRelay';
+import { astarConfig } from './astar';
 
 export const chainsConfigList: ChainConfig[] = [
   acalaConfig,
   alphanetRelayConfig,
+  astarConfig,
 ];
 
 export const chainsConfigMap = new Map<string, ChainConfig>(
