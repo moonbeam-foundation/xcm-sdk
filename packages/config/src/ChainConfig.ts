64e4f1f6fcf8cfcde4bfa04cb4644bdf34ab156c
Elmar Kenigs <elmar@purestake.com>
2023-05-09 16:00:35 +0300

    --wip-- [skip ci]

--- packages/config/src/ChainConfig.ts
+++ packages/config/src/ChainConfig.ts
@@ -7,12 +7,19 @@ export interface ChainConfigConstructorParams {
 }
 
 export class ChainConfig {
-  readonly assets: Map<string, AssetConfig>;
+  readonly assets: Map<string, AssetConfig[]> = new Map();
 
   readonly chain: Chain;
 
   constructor({ assets, chain }: ChainConfigConstructorParams) {
-    this.assets = new Map(assets.map((asset) => [asset.asset.key, asset]));
     this.chain = chain;
+
+    assets.forEach((asset) => {
+      const configs = this.assets.get(asset.asset.key) || [];
+
+      configs.push(asset);
+
+      this.assets.set(asset.asset.key, configs);
+    });
   }
 }
