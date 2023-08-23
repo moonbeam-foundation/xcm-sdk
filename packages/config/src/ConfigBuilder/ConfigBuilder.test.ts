6b723d0c6e065d3e79c3dbb8bbc62898b871a7a2
Pavol Noha <pavol.noha@gmail.com>
2023-08-23 20:43:51 +0200

    Mutable service integrity check test

--- packages/config/src/ConfigBuilder/ConfigBuilder.test.ts
+++ packages/config/src/ConfigBuilder/ConfigBuilder.test.ts
@@ -1,5 +1,6 @@
 /* eslint-disable sort-keys */
 import { Ecosystem } from '@moonbeam-network/xcm-types';
+import { MutableConfigService } from '../ConfigService';
 import { dev } from '../assets';
 import { equilibriumAlphanet, moonbaseAlpha } from '../chains';
 import { equilibriumAlphanetConfig } from '../configs/equilibriumAlphanet';
@@ -9,6 +10,34 @@ import { ConfigBuilder } from './ConfigBuilder';
 describe('configBuilder', () => {
   it('should return correct dev config', () => {
     const config = ConfigBuilder()
+      .assets(Ecosystem.AlphanetRelay)
+      .asset(dev)
+      .source(moonbaseAlpha)
+      .destination(equilibriumAlphanet)
+      .build();
+
+    expect(config).toStrictEqual({
+      asset: dev,
+      source: {
+        chain: moonbaseAlpha,
+        config: moonbaseAlphaConfig.getAssetDestinationConfig(
+          dev,
+          equilibriumAlphanet,
+        ),
+      },
+      destination: {
+        chain: equilibriumAlphanet,
+        config: equilibriumAlphanetConfig.getAssetDestinationConfig(
+          dev,
+          moonbaseAlpha,
+        ),
+      },
+    });
+  });
+
+  it('should return correct dev config using mutable service', () => {
+    const configService = new MutableConfigService();
+    const config = ConfigBuilder(configService)
       .assets(Ecosystem.AlphanetRelay)
       .asset(dev)
       .source(moonbaseAlpha)
