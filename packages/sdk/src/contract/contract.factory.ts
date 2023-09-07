0fec36e8aa41d8fb794e664995cadc935a1f97a8
Mario J Maurello <marioj@purestake.com>
2023-09-07 02:27:23 +0200

    fix type

--- packages/sdk/src/contract/contract.factory.ts
+++ packages/sdk/src/contract/contract.factory.ts
@@ -1,6 +1,5 @@
 import { ContractConfig } from '@moonbeam-network/xcm-builder';
-import { Signer } from 'ethers';
-import { WalletClient } from 'viem';
+import { EvmSigner } from '../sdk.interfaces';
 import {
   BalanceContractInterface,
   TransferContractInterface,
@@ -9,7 +8,7 @@ import { Erc20, Xtokens } from './contracts';
 
 export function createContract(
   config: ContractConfig,
-  signer: Signer | WalletClient,
+  signer: EvmSigner,
 ): TransferContractInterface | BalanceContractInterface {
   if (config.module === 'Erc20') {
     return new Erc20(config, signer);
