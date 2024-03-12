7e24392a67733b91c1fc7cc23543c74a88f36ed2
mmaurello <93129175+mmaurello@users.noreply.github.com>
2024-03-13 05:03:40 +1300

    PINK integration with Moonbeam (#212)

--- packages/config/src/configs/polkadotAssetHub.ts
+++ packages/config/src/configs/polkadotAssetHub.ts
@@ -4,7 +4,7 @@ import {
   ExtrinsicBuilder,
   FeeBuilder,
 } from '@moonbeam-network/xcm-builder';
-import { dot, usdc, usdt } from '../assets';
+import { dot, pink, usdc, usdt } from '../assets';
 import { moonbeam, polkadotAssetHub } from '../chains';
 import { AssetConfig } from '../types/AssetConfig';
 import { ChainConfig } from '../types/ChainConfig';
@@ -40,6 +40,26 @@ export const polkadotAssetHubConfig = new ChainConfig({
       destinationFee: {
         amount: 0.03,
         asset: usdc,
+        balance: BalanceBuilder().substrate().assets().account(),
+      },
+      extrinsic: ExtrinsicBuilder()
+        .polkadotXcm()
+        .limitedReserveTransferAssets()
+        .X2(),
+      fee: {
+        asset: dot,
+        balance: BalanceBuilder().substrate().system().account(),
+        xcmDeliveryFeeAmount,
+      },
+      min: AssetMinBuilder().assets().asset(),
+    }),
+    new AssetConfig({
+      asset: pink,
+      balance: BalanceBuilder().substrate().assets().account(),
+      destination: moonbeam,
+      destinationFee: {
+        amount: FeeBuilder().assetManager().assetTypeUnitsPerSecond(),
+        asset: usdt,
         balance: BalanceBuilder().substrate().assets().account(),
       },
       extrinsic: ExtrinsicBuilder()
