bf0e917e64372b7a9bb7782ff8bc8ea69149bd36
mmaurello <93129175+mmaurello@users.noreply.github.com>
2024-03-20 02:03:48 +1300

    fix Hydra decimals as Options (#220)
    * fix Hydra decimals as Options
    
    * add changeset

--- packages/sdk/src/polkadot/PolkadotService.ts
+++ packages/sdk/src/polkadot/PolkadotService.ts
@@ -125,9 +125,13 @@ export class PolkadotService {
 
     const data = (await fn(asset)) as AssetMetadata | Option<AssetMetadata>;
     const unwrapped = 'unwrapOrDefault' in data ? data.unwrapOrDefault() : data;
+    const decimals =
+      'unwrapOrDefault' in unwrapped.decimals
+        ? unwrapped.decimals.unwrapOrDefault()
+        : unwrapped.decimals;
 
     return {
-      decimals: unwrapped.decimals.toNumber(),
+      decimals: decimals.toNumber(),
       symbol: unwrapped.symbol.toString(),
     };
   }
@@ -149,12 +153,17 @@ export class PolkadotService {
   async getAssetDecimals(asset: Asset): Promise<number> {
     const metaId = this.chain.getMetadataAssetId(asset);
 
-    return (
+    const decimals =
       this.chain.getAssetDecimals(asset) ||
       (await this.getAssetDecimalsFromQuery(metaId)) ||
       (await this.getAssetMeta(metaId))?.decimals ||
-      this.decimals
-    );
+      this.decimals; // TODO remove this and handle it separately only for native assets
+
+    if (!decimals) {
+      throw new Error(`No decimals found for asset ${asset.originSymbol}`);
+    }
+
+    return decimals;
   }
 
   async query(config: SubstrateQueryConfig): Promise<bigint> {
