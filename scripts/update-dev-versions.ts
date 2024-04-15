c1167fe1813f169c359ca0b05b7a46fe068f1789
Richard Kenigs <riha@opslayer.com>
2024-04-15 11:12:42 +0200

    Improve Dev version script, fix acc tets, add more info to the pipeline

--- scripts/update-dev-versions.ts
+++ scripts/update-dev-versions.ts
@@ -10,6 +10,23 @@ import { promisify } from 'node:util';
 
 const execPromise = promisify(exec);
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function getHighestDevTag(pkgs: any[]) {
+  const promises = pkgs.map(async (pkg) => {
+    const { name } = pkg;
+    const { stdout } = await execPromise(`npm view ${name} dist-tags.dev`);
+    const current = stdout.replace(/\n$/, '');
+
+    const number = current.split('.').at(-1) || '0';
+
+    return Number(number);
+  });
+
+  const allVersions = await Promise.all(promises);
+
+  return Math.max(...allVersions);
+}
+
 async function main() {
   const files = await glob(`${process.env.PWD}/packages/**/package.json`, {
     ignore: '**/node_modules/**',
@@ -21,13 +38,18 @@ async function main() {
     }),
   );
 
+  const highestCurrent = await getHighestDevTag(pkgs);
+  const nextVersionNumber = highestCurrent + 1;
+
+  console.log(`highestCurrent = ${highestCurrent}`);
+  console.log(`newVersion = ${nextVersionNumber}`);
+
   for (const pkg of pkgs) {
     const { name } = pkg;
     const { stdout } = await execPromise(`npm view ${name} dist-tags.dev`);
+
     const current = stdout.replace(/\n$/, '');
-    const number = current.split('.').at(-1) || '0';
-    const next = Number(number) + 1;
-    const newVersion = current.replace(/\.\d+$/, `.${next}`);
+    const newVersion = current.replace(/\.\d+$/, `.${nextVersionNumber}`);
 
     pkg.version = newVersion;
 
