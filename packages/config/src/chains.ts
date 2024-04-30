7dda4ee698ca845c839f4fd8e7cab7fdd5d1ae0c
Richard Kenigs <riha@opslayer.com>
2024-04-30 14:25:57 +0200

    add peaq alphanet

--- packages/config/src/chains.ts
+++ packages/config/src/chains.ts
@@ -485,6 +485,35 @@ export const hydraDxAlphanet = new Parachain({
   ws: 'wss://hydradx-moonbase-rpc.play.hydration.cloud',
 });
 
+export const peaqAlphanet = new Parachain({
+  assetsData: [
+    {
+      asset: usdcwh,
+      decimals: 6,
+      id: 2816,
+    },
+    {
+      asset: dev,
+      decimals: 18,
+      id: 2560,
+    },
+    {
+      asset: ftmwh,
+      decimals: 18,
+      id: 1001,
+    },
+  ],
+  ecosystem: Ecosystem.AlphanetRelay,
+  genesisHash:
+    '0x2dfcd5c560f6db1667cbc2bc3791dfd337f88f400af6de39b1b8638ee7af6ed4',
+  isTestChain: true,
+  key: 'peaq-Alphanet',
+  name: 'Peaq Alphanet',
+  parachainId: 3013,
+  ss58Format: 42,
+  ws: 'wss://archive-moonbeam.peaq.network',
+});
+
 export const interlay = new Parachain({
   assetsData: [
     {
