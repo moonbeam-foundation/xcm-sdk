22d9d8d32a9d962b91c60178f3af037f558319f9
Mario J Maurello <marioj@purestake.com>
2024-06-04 21:51:13 +0900

    re-adjust PEN fee

--- packages/config/src/configs/moonbeam.ts
+++ packages/config/src/configs/moonbeam.ts
@@ -417,7 +417,7 @@ export const moonbeamConfig = new ChainConfig({
       contract: ContractBuilder().Xtokens().transfer(),
       destination: pendulum,
       destinationFee: {
-        amount: 1,
+        amount: 1.01,
         asset: pen,
         balance: BalanceBuilder().substrate().assets().account(),
       },
