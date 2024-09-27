d0fcf25c734f15df5c8a276425fc33320888e390
Mario J Maurello <marioj@purestake.com>
2024-09-27 19:32:39 +0200

    add setup for dev in fantom > moonbase route

--- packages/config/src/mrl-configs/fantomTestnet.ts
+++ packages/config/src/mrl-configs/fantomTestnet.ts
@@ -52,7 +52,7 @@ export const fantomTestnetRoutes = new ChainRoutes({
         balance: BalanceBuilder().evm().erc20(),
         fee: {
           asset: ftmwh,
-          amount: 0.01,
+          amount: 0,
         },
       },
       mrl: {
@@ -60,6 +60,37 @@ export const fantomTestnetRoutes = new ChainRoutes({
         transfer: MrlBuilder().wormhole().wormhole().tokenTransfer(),
         moonChain: {
           asset: ftmwh,
+          fee: {
+            asset: dev,
+            amount: 0.1,
+            balance: BalanceBuilder().substrate().system().account(),
+          },
+        },
+      },
+    },
+    {
+      source: {
+        asset: dev,
+        balance: BalanceBuilder().evm().erc20(),
+        destinationFee: {
+          asset: dev,
+          balance: BalanceBuilder().evm().erc20(),
+        },
+      },
+      destination: {
+        asset: dev,
+        chain: moonbaseAlpha,
+        balance: BalanceBuilder().substrate().system().account(),
+        fee: {
+          asset: dev,
+          amount: 0,
+        },
+      },
+      mrl: {
+        isAutomatic: false,
+        transfer: MrlBuilder().wormhole().wormhole().tokenTransfer(),
+        moonChain: {
+          asset: dev,
           fee: {
             asset: dev,
             amount: 0.1,
