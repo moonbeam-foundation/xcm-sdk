1eb642d13ecfcc2614a68d32eb82a74529c75b16
mmaurello <marioj@opslayer.com>
2025-03-18 09:37:05 +0100

    remove script

--- scripts/idToAddress.ts
+++
@@ -1,7 +0,0 @@
-import { formatAssetIdToERC20 } from '../packages/utils/src';
-
-const id = '294342517635293430510451841925413680520';
-
-// tsx scripts/idToAddress.ts
-console.log(formatAssetIdToERC20(id));
-// //                 const hash = send(min, (event) => {
