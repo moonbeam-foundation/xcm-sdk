be1481863b2e8d22fa799adcb745a8e8a29249ac
mmaurello <marioj@opslayer.com>
2025-05-28 16:40:12 +0200

    separate cross ecosystem bridge to another route map

---
+++ packages/config/src/cross-ecosystem-configs/moonbaseStage.ts
@@ -0,0 +1,64 @@
+import {
+  BalanceBuilder,
+  ExtrinsicBuilder,
+} from '@moonbeam-network/xcm-builder';
+import { devBeta, devStage } from '../assets';
+import { moonbaseBeta, moonbaseStage } from '../chains';
+import { ChainRoutes } from '../types/ChainRoutes';
+
+export const moonbaseStageRoutes = new ChainRoutes({
+  chain: moonbaseStage,
+  routes: [
+    {
+      source: {
+        asset: devStage,
+        balance: BalanceBuilder().substrate().system().account(),
+        fee: {
+          asset: devStage,
+          balance: BalanceBuilder().substrate().system().account(),
+        },
+        destinationFee: {
+          balance: BalanceBuilder().evm().erc20(),
+        },
+      },
+      destination: {
+        asset: devStage,
+        chain: moonbaseBeta,
+        balance: BalanceBuilder().evm().erc20(),
+        fee: {
+          amount: 0.1, // TODO
+          asset: devStage,
+          balance: BalanceBuilder().evm().erc20(),
+        },
+      },
+      extrinsic: ExtrinsicBuilder()
+        .polkadotXcm()
+        .transferAssetsToEcosystem()
+        .X1(),
+    },
+    {
+      source: {
+        asset: devBeta,
+        balance: BalanceBuilder().evm().erc20(),
+        fee: {
+          asset: devStage,
+          balance: BalanceBuilder().evm().native(),
+        },
+      },
+      destination: {
+        asset: devBeta,
+        chain: moonbaseBeta,
+        balance: BalanceBuilder().substrate().system().account(),
+        fee: {
+          asset: devBeta,
+          amount: 0.1, // TODO
+          balance: BalanceBuilder().substrate().system().account(),
+        },
+      },
+      extrinsic: ExtrinsicBuilder()
+        .polkadotXcm()
+        .transferAssetsToEcosystem()
+        .X3(),
+    },
+  ],
+});
