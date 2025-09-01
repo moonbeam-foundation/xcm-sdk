70f038518a04b193689a8001b2f3e4d5bab0634a
Richard Kenigs <riha@opslayer.com>
2025-09-01 14:48:27 +0200

    try to split config

--- packages/config/tsup.config.ts
+++ packages/config/tsup.config.ts
@@ -1,4 +1,21 @@
 import { defineConfig } from 'tsup';
 import { baseConfig } from '../../tsup.config';
 
-export default defineConfig(baseConfig);
+export default defineConfig({
+  ...baseConfig,
+  splitting: true,
+  entry: {
+    index: 'src/index.ts',
+    assets: 'src/assets.ts',
+    chains: 'src/chains.ts',
+    'config-utils': 'src/config.utils.ts',
+    'config-service': 'src/ConfigService/index.ts',
+    'cross-ecosystem-configs': 'src/cross-ecosystem-configs/index.ts',
+    'mrl-configs': 'src/mrl-configs/index.ts',
+    'types/asset-route': 'src/types/AssetRoute.ts',
+    'types/chain-routes': 'src/types/ChainRoutes.ts',
+    'types/mrl-asset-route': 'src/types/MrlAssetRoute.ts',
+    'types/mrl-chain-routes': 'src/types/MrlChainRoutes.ts',
+    'xcm-configs': 'src/xcm-configs/index.ts',
+  },
+});
