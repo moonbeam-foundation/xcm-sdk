b0781e9be148c89741602efd0476b8a5e434e8b3
mmaurello <marioj@opslayer.com>
2025-11-13 11:16:01 +0100

    add tests for fee calculation query

--- packages/builder/src/fee/FeeBuilder.ts
+++ packages/builder/src/fee/FeeBuilder.ts
@@ -1,46 +1,11 @@
-import type { u128 } from '@polkadot/types';
-import { SubstrateQueryConfig } from '../types';
-import type { BridgeFeeConfigBuilder } from './FeeBuilder.interfaces';
 import { gateway } from './gateway';
+import { outboundQueueApi } from './outboundQueueApi';
 import { xcmPaymentApi } from './xcmPaymentApi';
 
 export function FeeBuilder() {
   return {
-    xcmPaymentApi,
     gateway,
     outboundQueueApi,
-  };
-}
-
-// TODO mjm tests to this
-function outboundQueueApi() {
-  return {
-    calculateFee: (): BridgeFeeConfigBuilder => {
-      return {
-        build: ({ address, asset, balance }) => {
-          const args = [
-            {
-              MintForeignToken: {
-                tokenId: asset.getAssetId(),
-                recipient: address,
-                amount: balance?.amount,
-              },
-            },
-            null,
-          ];
-          return new SubstrateQueryConfig({
-            module: 'outboundQueueApi',
-            func: 'calculateFee',
-            args,
-            queryType: 'call',
-            transform: async (data: {
-              local: u128;
-              remote: u128;
-            }): Promise<bigint> =>
-              data.local.toBigInt() + data.remote.toBigInt(),
-          });
-        },
-      };
-    },
+    xcmPaymentApi,
   };
 }
---
+++ packages/builder/src/fee/outboundQueueApi/outboundQueueApi.ts
@@ -0,0 +1,36 @@
+import type { u128 } from '@polkadot/types';
+import { SubstrateQueryConfig } from '../../types';
+import type { BridgeFeeConfigBuilder } from '../FeeBuilder.interfaces';
+
+// TODO mjm tests to this
+export function outboundQueueApi() {
+  return {
+    calculateFee: (): BridgeFeeConfigBuilder => {
+      return {
+        build: ({ address, asset, balance }) => {
+          const args = [
+            {
+              MintForeignToken: {
+                tokenId: asset.getAssetId(),
+                recipient: address,
+                amount: balance?.amount,
+              },
+            },
+            null,
+          ];
+          return new SubstrateQueryConfig({
+            module: 'outboundQueueApi',
+            func: 'calculateFee',
+            args,
+            queryType: 'call',
+            transform: async (data: {
+              local: u128;
+              remote: u128;
+            }): Promise<bigint> =>
+              data.local.toBigInt() + data.remote.toBigInt(),
+          });
+        },
+      };
+    },
+  };
+}
